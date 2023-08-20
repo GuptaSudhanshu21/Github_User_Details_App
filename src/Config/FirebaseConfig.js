22efa6e2effdfe173e992c9a074e9065be61a78a
GuptaSudhanshu21 <guptasudhanshu2001@gmail.com>
2023-08-21 00:43:27 +0530

    complete project

---
+++ src/Config/FirebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCDEwGAGhpyqcIyUqSkub7z_fBgGbwBMHQ",
+  authDomain: "mygitapp-3ec29.firebaseapp.com",
+  projectId: "mygitapp-3ec29",
+  storageBucket: "mygitapp-3ec29.appspot.com",
+  messagingSenderId: "559203560807",
+  appId: "1:559203560807:web:9056a17ba021eac6c553c2",
+  measurementId: "G-5MW3QL5TTY",
+};
+export default firebaseConfig;
